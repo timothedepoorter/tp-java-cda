da4baab8ef8697de9c2461d7e5aa14276c863c88
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2023-12-11 14:27:45 +0100

    init

---
+++ src/main/java/fr/exercices/Exo1.java
@@ -0,0 +1,33 @@
+package fr.exercices;
+
+public class Exo1 {
+
+    /**
+     * @param numbers array of int
+     * @return true if numbers between 0 and 20, false instead
+     */
+    public static boolean verifyIfNumbersBetween0and20(int[] numbers) {
+        for (int number : numbers) {
+            if (number < 0 || number > 20) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * @param numbers array of int
+     * @return the average of the array
+     */
+    public static int calculateAverageOfNumbers(int[] numbers) {
+        if (!verifyIfNumbersBetween0and20(numbers)) {
+            return 0;
+        }
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        return total / numbers.length;
+    }
+}
---
+++ src/main/java/fr/exercices/Main.java
@@ -0,0 +1,8 @@
+package fr.exercices;
+
+public class Main {
+    public static void main (String[] args) {
+        int[] notes = {10, 10, 10};
+        System.out.println("La moyenne de ces 3 notes est de : " + Exo1.calculateAverageOfNumbers(notes));
+    }
+}
